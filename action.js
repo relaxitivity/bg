c86b19ac0bb52c206f7c4275337f1089163089fd
relaxitivity <98897637+relaxitivity@users.noreply.github.com>
2022-02-02 20:51:41 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,16 @@
+function changetoRed(){
+    document.body.style.backgroundColor="red";
+}
+function changetoOrange(){
+    
+    document.body.style.backgroundColor="orange";
+}
+function changetoGreen(){
+    
+    document.body.style.backgroundColor="green";
+}
+function changetoYellow(){
+    
+    document.body.style.backgroundColor="yellow";
+}
+
